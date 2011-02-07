52bb2e31b05e19cef02fbc4713ac52e6044c3693
Maciek Makowski <maciek.makowski@gmail.com>
2011-02-07 16:23:03 +0000

    do not attempt to find absolute java path if no JDK information is available

--- src/main/java/org/jvnet/hudson/plugins/SbtPluginBuilder.java
+++ src/main/java/org/jvnet/hudson/plugins/SbtPluginBuilder.java
@@ -128,10 +128,14 @@ public class SbtPluginBuilder extends Builder {
 		}
 
 		// java
-		String java = build.getProject().getJDK() != null ? build.getProject()
-				.getJDK().getBinDir()
-				+ "/java" : "java";
-		args.add(new File(java).getAbsolutePath());
+		String javaExePath;
+		if (build.getProject().getJDK() != null) {
+			javaExePath = new File(build.getProject().getJDK().getBinDir()
+					+ "/java").getAbsolutePath();
+		} else {
+			javaExePath = "java";
+		}
+		args.add(javaExePath);
 
 		String[] split = jvmFlags.split(" ");
 		for (String flag : split) {
